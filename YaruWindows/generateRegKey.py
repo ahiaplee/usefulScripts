651e6133ea8d8ecbe44215b42608ad47d93ee11f
Hiap Lee Ang <anghiaplee@gmail.com>
2024-09-07 17:51:30 +0800

    tweaks to yaru

--- YaruWindows/generateRegKey.py
+++ YaruWindows/generateRegKey.py
@@ -1,7 +1,16 @@
 import sys
 import os
 
+def decimal_to_reg_dword(decimal_value):
+    # Convert decimal to hexadecimal with 8 digits, zero-padded
+    hex_value = format(decimal_value, '08x')
+    # Return the formatted string
+    return f'dword:{hex_value}'
+
 fileContents = '''Windows Registry Editor Version 5.00
+
+[HKEY_CURRENT_USER\Control Panel\Cursors]
+"CursorBaseSize"={}
 
 [HKEY_CURRENT_USER\Control Panel\Cursors\Schemes]
 "YaruWindows"="{}"
@@ -12,7 +21,8 @@ if __name__ == "__main__":
     cursors = f"{cursorPath}\\\\arrow.cur,{cursorPath}\\\\whats_this.cur,{cursorPath}\\\\half-busy.ani,{cursorPath}\\\\wait.ani,{cursorPath}\\\\cross.cur,{cursorPath}\\\\text.cur,{cursorPath}\\\\pencil.cur,{cursorPath}\\\\not-allowed.cur,{cursorPath}\\\\size_ver.cur,{cursorPath}\\\\size_hor.cur,{cursorPath}\\\\size-fdiag.cur,{cursorPath}\\\\size-bdiag.cur,{cursorPath}\\\\move.cur,{cursorPath}\\\\sb_up_arrow.cur,{cursorPath}\\\\hand.cur"
     
     filename = 'AddYaruWindowsCursorTheme.reg'
+    fontSize = 32
     with open(filename, 'w') as file:
-        file.write(fileContents.format(cursors))
+        file.write(fileContents.format(decimal_to_reg_dword(fontSize), cursors))
         
     print(f"Created {filename}")
